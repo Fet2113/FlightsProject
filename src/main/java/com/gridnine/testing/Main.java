334fec91a08285bc775c4b873c33b2cca135855d
Fet2113 <a2113@inbox.ru>
2025-06-14 11:54:33 +0300

    Повторно выполнено задание по фильтрации перелетов. Учтены замечания.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,31 +1,160 @@
 package com.gridnine.testing;
 
-
-import com.gridnine.testing.factory.FlightBuilder;
-import com.gridnine.testing.filter.ArrivalEarlierDepartureFilter;
-import com.gridnine.testing.filter.DepartureBeforeNowFilter;
-import com.gridnine.testing.filter.TwoHoursGroundTimeFilter;
-import com.gridnine.testing.model.Flight;
-import com.gridnine.testing.service.FlightService;
-
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class Main {
-    public static void main(String[] args) {
-        List<Flight> flightsArray = FlightBuilder.createFlights();
 
-        System.out.println(flightsArray);
+class FlightBuilder {
+    public static List<Flight> createFlights() {
+        LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
+        return Arrays.asList(
+                //A normal flight with two hour duration
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2)),
+                //A normal multi segment flight
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
+                //A flight departing in the past
+                createFlight(threeDaysFromNow.minusDays(6), threeDaysFromNow),
+                //A flight that departs before it arrives
+                createFlight(threeDaysFromNow, threeDaysFromNow.minusHours(6)),
+                //A flight with more than two hours ground time
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
+                //Another flight with more than two hours ground time
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
+                        threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
+    }
 
-        FlightService fService1 = new FlightService(List.of(new DepartureBeforeNowFilter()));
-        System.out.println(fService1.filterFlights(flightsArray));
-
-        FlightService fService2 = new FlightService(List.of(new ArrivalEarlierDepartureFilter()));
-        System.out.println(fService2.filterFlights(flightsArray));
-
-        FlightService fService3 = new FlightService(List.of(new TwoHoursGroundTimeFilter()));
-        System.out.println(fService3.filterFlights(flightsArray));
-
-        FlightService fService4 = new FlightService(List.of(new TwoHoursGroundTimeFilter(), new DepartureBeforeNowFilter(), new ArrivalEarlierDepartureFilter()));
-        System.out.println(fService4.filterFlights(flightsArray));
+    private static Flight createFlight(final LocalDateTime... dates) {
+        if ((dates.length % 2) != 0) {
+            throw new IllegalArgumentException(
+                    "you must pass an even number of dates");
+        }
+        List<Segment> segments = new ArrayList<>(dates.length / 2);
+        for (int i = 0; i < (dates.length - 1); i += 2) {
+            segments.add(new Segment(dates[i], dates[i + 1]));
+        }
+        return new Flight(segments);
     }
 }
+
+class Flight {
+    private final List<Segment> segments;
+
+    Flight(final List<Segment> segs) {
+        segments = segs;
+    }
+
+    List<Segment> getSegments() {
+        return segments;
+    }
+
+    @Override
+    public String toString() {
+        return segments.stream().map(Object::toString)
+                .collect(Collectors.joining(" "));
+    }
+}
+
+
+class Segment {
+    private final LocalDateTime departureDate;
+
+    private final LocalDateTime arrivalDate;
+
+    Segment(final LocalDateTime dep, final LocalDateTime arr) {
+        departureDate = Objects.requireNonNull(dep);
+        arrivalDate = Objects.requireNonNull(arr);
+    }
+
+    LocalDateTime getDepartureDate() {
+        return departureDate;
+    }
+
+    LocalDateTime getArrivalDate() {
+        return arrivalDate;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter fmt =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        return '[' + departureDate.format(fmt) + '|' + arrivalDate.format(fmt)
+                + ']';
+    }
+}
+
+interface flightFilter {
+    Predicate<Flight> filter();
+}
+// * Сегменты с датой прилёта до текущего момента времени
+class DepartureUpToCurrentMoment implements flightFilter {
+    @Override
+    public Predicate<Flight> filter() {
+
+        return flight -> flight.getSegments()
+                .stream()
+                .anyMatch(segment -> segment.getDepartureDate().isBefore(LocalDateTime.now()));
+    }
+}
+
+// * Сегменты с датой прилёта раньше даты вылета
+class SegmentsWithArrivalDateBeforeDepartureDate implements flightFilter {
+    @Override
+    public Predicate<Flight> filter() {
+
+        return flight -> flight.getSegments()
+                .stream()
+                .anyMatch(segment -> segment.getArrivalDate().isBefore(segment.getDepartureDate()));
+    }
+}
+
+//        * Перелеты где общее время на земле больше 2 часов
+class FlightsWhereTheTotalTimeOnTheGroundIsMoreThanTwoHours implements flightFilter {
+    @Override
+    public Predicate<Flight> filter() {
+        return flight -> {
+            long timeOnEarth = 0;
+            List<Segment> segments = flight.getSegments();
+            for (var i = 0; i < flight.getSegments().size() - 1; i++) {
+
+                timeOnEarth += Duration.between(segments.get(i).getArrivalDate(), segments.get(i + 1).getDepartureDate())
+                        .toMillis();
+            }
+
+            return timeOnEarth < 2 * 60 * 60 * 1000;
+        };
+    }
+}
+
+
+class FlightMain {
+    public static void main(String[] args) {
+        List<Flight> flights = FlightBuilder.createFlights();
+
+        System.out.println("Общая выборка полётов");
+        flights.forEach(flight -> System.out.println(flight.toString()));
+
+        System.out.println("Вылет до текущего момента времени");
+        flights.stream().filter(new DepartureUpToCurrentMoment().filter())
+                .forEach(f -> System.out.println(f.toString()));
+
+        System.out.println("Сегменты с датой прилёта раньше даты вылета");
+        flights.stream().filter(new SegmentsWithArrivalDateBeforeDepartureDate().filter())
+                .forEach(f -> System.out.println(f.toString()));
+
+        System.out.println("Перелеты где общее время на земле больше 2 часов");
+        flights.stream().filter(new FlightsWhereTheTotalTimeOnTheGroundIsMoreThanTwoHours().filter())
+                .forEach(f -> System.out.println(f.toString()));
+    }
+}
+
+
